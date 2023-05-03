32681ce219668888316d59646e60e848abedeffa
Hillpro <66534835+Hillpro@users.noreply.github.com>
2023-05-03 18:10:14 -0400

    bindings

--- src/dataset_viewer/viewer.py
+++ src/dataset_viewer/viewer.py
@@ -1,6 +1,41 @@
 import napari
+from napari import Viewer as NapariViewer, layers
+from vispy.util import keys
 
-def start():
-    viewer = napari.Viewer()
+from dataset_viewer.dataset import Dataset
 
-    napari.run()
+
+class Viewer():
+    dataset = Dataset()
+
+    def __init__(self):
+        self.viewer = NapariViewer()
+        self.__set_bindings()
+
+    def start(self):
+        napari.run()
+
+    def __set_bindings(self):
+        self.__bind_key('h', self.__hello)
+        self.__bind_key('l', self.__current_label_name)
+        self.__bind_key('Escape', self.__exit)
+
+    def __bind_key(self, key, func):
+        self.viewer.bind_key(key, func)
+
+    def __current_label_name(self, viewer: NapariViewer):
+        active_layer = viewer.layers.selection.active
+
+        if active_layer is not None:
+            print(viewer.layers.selection.active.name)
+
+    def __hello(self, viewer: NapariViewer):
+        print('Hello World!')
+        yield
+        print('Bye World!')
+
+    def __exit(self, viewer: NapariViewer):
+        viewer.close()
+
+
+Viewer().start()
